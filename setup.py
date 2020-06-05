0b130e16166c0fba39cff48e397c336fa1765337
Filipe Waitman <filwaitman@gmail.com>
2020-06-05 14:13:12 -0300

    Update repo according to my personal template
    https://github.com/filwaitman/cookiecutter-lib

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+import sys
+
 from setuptools import setup
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write('This project does not support your Python version.')
+    sys.exit(1)
 
 setup()
